2c58dff5462c8f854ef3487b6b42a4b3675b4d17
Boistean-Sergiu <boistean.sergiu@gmail.com>
2017-06-07 12:59:08 +0300

    Add files via upload

---
+++ html pages/gasite.js
@@ -0,0 +1,11 @@
+template.list_items.all_items = function() 
+		{
+			electronice.find();
+			animale.find();
+			haine.find();
+			bijuterii.find();
+			tichete.find();
+			accesorii.find();
+			bani.find();
+			jucarii.find();
+		}
